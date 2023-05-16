2101a17f68be1e08fe8f1afde32d1e7cae5c829a
Amrnash <amrnashaat8@gmail.com>
2023-05-17 01:44:28 +0300

    Use the error-handler middleware and handle notfound routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -11,7 +14,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+app.all("*", () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000??");
+  console.log("Listening on port 3000");
 });
